9855ae26db0086c06b9b3c546cd3083bc9d72c54
Bianca <ing_biank@hotmail.com>
2018-12-01 11:23:19 +0100

    adds 1:m association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
                     length: { maximum: 245 }, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  has_many :recipes                    
 end
