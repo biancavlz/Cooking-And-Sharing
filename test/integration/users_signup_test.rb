31d0fdb11c1c70c00ca5464f82485a4af43b5b50
Bianca <ing_biank@hotmail.com>
2018-12-05 13:14:44 +0100

    creates CREATE action for users

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,4 +5,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test "reject an invalid signup" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, params: { user: { name: " ", email: " ", password: " ", password_confirmation: " " } }
+    end
+    assert_template "users/new"
+    assert_select "h4.alert-heading"
+  end
+
+  test "accept valid signup" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { 
+        name: "John", 
+        email: "john@example.com", 
+        password: "password", 
+        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+  end
 end
