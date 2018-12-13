6a7e0a1ffb641198d3edde9e74d4b4158d7541e9
Bianca <ing_biank@hotmail.com>
2018-12-13 15:37:30 +0100

    creates ingreditents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_102500) do
+ActiveRecord::Schema.define(version: 2018_12_13_143525) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
