369e96348958e95838a16022bdbf2ca8b43ea888
Bianca <ing_biank@hotmail.com>
2018-12-21 12:16:05 +0100

    Adds comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_13_150040) do
+ActiveRecord::Schema.define(version: 2018_12_21_111146) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
