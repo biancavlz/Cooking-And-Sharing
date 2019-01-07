cf3d2fa9d636807d9776c1828f2fd5b716d64800
Bianca <ing_biank@hotmail.com>
2019-01-07 16:34:10 +0100

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  mount ActionCable.sever => '/cable'
+  mount ActionCable.server => '/cable'
 end
