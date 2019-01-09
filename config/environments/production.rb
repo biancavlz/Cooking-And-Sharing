19172f3c31c937d767bd7a62f50c23ffc46f60d5
Bianca <ing_biank@hotmail.com>
2019-01-09 16:02:52 +0100

    Adds whole set up for redis in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://cooking-and-sharing.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://cooking-and-sharing.herokuapp.com', 'http://cooking-and-sharing.herokuapp.com']
 end
