5235ce41881e538daba10f433d403752247bfe6c
Bianca <ing_biank@hotmail.com>
2019-01-20 12:09:32 +0100

    Adds thumbs_up / down count methods in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,4 +10,12 @@ class Recipe < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   default_scope -> { order(updated_at: :desc) }
+
+  def thumbs_up_total
+    self.likes.where(like: true).size
+  end
+
+  def thumbs_down_total
+    self.likes.where(like: false).size    
+  end
 end
